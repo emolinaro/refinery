0aa850ed0814dfa5452d0786be55d999ee2f2217
Emiliano Molinaro <emil.molinaro@gmail.com>
2026-09-20 20:27:38 +0200

    Release v0.2.2: Electron selection-capture fix

--- Scripts/build-app.sh
+++ Scripts/build-app.sh
@@ -36,9 +36,9 @@ cat > "$STAGING_APP/Contents/Info.plist" <<'PLIST'
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleShortVersionString</key>
-    <string>0.2.1</string>
+    <string>0.2.2</string>
     <key>CFBundleVersion</key>
-    <string>6</string>
+    <string>7</string>
     <key>LSMinimumSystemVersion</key>
     <string>13.0</string>
     <key>LSUIElement</key>
